410a21c5bd71c6db407e06baf4e045237fdd3812
IrPedro <irpedrogsp@usp.br>
2026-08-24 05:14:37 -0300

    arruma erros de nome  dos audios

--- src/App.svelte
+++ src/App.svelte
@@ -24,7 +24,7 @@
   let instrumentVolume = 80; // 80%
   let isMusicEnabled = true; // controle do botão de mudo
   $: if (bgmAudio) bgmAudio.volume = bgmVolume / 100;
-  const BGM_MENU = './audio/Buy Something!.mp3';   // música do menu
+  const BGM_MENU = './audio/Buy_Something!.mp3';   // música do menu
   const BGM_GAME = './audio/two_left_socks.ogg';     // música do jogo
   let currentBgm = BGM_MENU;  // inicialmente aponta para o menu
 
